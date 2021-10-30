993fbe82ab1d3df7ce4a6591094ebb26d7efec5d
Sushanta Gupta <sushanta.gupta007@gmail.com>
2021-10-30 05:36:09 -0700

    check before heroku

--- index.js
+++ index.js
@@ -13,19 +13,44 @@ app.use(cors())
 app.use(express.json())
 
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m1cep.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-    console.log("from database")
-  client.close();
-});
+
+
+async function run() {
+  try{
+    await client.connect();
+    const database = client.db("Travel-Agent");
+    const dataTable = database.collection("Services")
+
+    //Table Find
+   
+
+    app.get('/Services',async(req,res)=>{
+        const myDoc = await dataTable.find({});
+      
+        const result = await myDoc.toArray(); 
+        res.send(result)
+
+    })
+  }
+  finally{}
+}
+
+run().catch()
+
+
 
 app.get('/user',(req,res)=>{
     res.send("I am server")
 })
 
+
+app.get('/',(req,res)=>{
+  res.send("renning")
+  console.log("running server")
+})
 app.listen(port,()=>{
     console.log("listening port",port)
 })
