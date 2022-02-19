9f42ef16bdd1dfcfb75f52c3432234d042381e17
Sushanta Gupta <sushanta.gupta007@gmail.com>
2022-02-19 02:44:26 -0800

    server code added

--- index.js
+++ index.js
@@ -2,10 +2,76 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => {
+const cors = require('cors')
+
+
+
+
+app.use(cors())
+app.use(express.json())
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Travel:wCKngdlWYOH0Gq9I@cluster0.7jwba.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("ExplorePlace");
+    const places = database.collection("Description");
+    const blogs = database.collection("BlogData"); 
+    const subscriber = database.collection("subscriber");
+    //Get API
+    app.get('/places',async(req,res)=>{
+      const placeData = places.find({})
+      const placeArray = await placeData.toArray()
+      res.send(placeArray)
+     
+    })
+    app.get('/blogs',async(req,res)=>{
+      const blogDes = blogs.find({})
+      const blogArray = await blogDes.toArray()
+      res.send(blogArray)
+      
+    })
+    app.post('/subscriber',async(req,res)=>{
+      const data = req.body
+      const subscriber = database.collection("subscriber");
+
+      const doc = {
+        name: data.firstName,
+        email:data.email
+      }
+      const result = await subscriber.insertOne(doc);
+
+      console.log(result.insertedId)
+      res.send(doc)
+    })
+    
+    app.get('/subscriber',async(req,res)=>{
+        const subscriberData = subscriber.find({})
+        const subscriberArray = await subscriberData.toArray()
+        res.send(subscriberArray)
+    })
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch();
+
+app.get('/user', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/post', function (req, res) {
+    res.send('Got a POST request')
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+//Travel
+//wCKngdlWYOH0Gq9I
